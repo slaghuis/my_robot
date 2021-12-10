1d6b62126b3f26cea61f8cf3120dde2fc2dd3a45
Eric Slaghuis <eslaghuis@gmail.com>
2021-12-10 12:02:05 +0200

    Safety Precautions

--- launch/fly.launch.py
+++ launch/fly.launch.py
@@ -144,7 +144,7 @@ def generate_launch_description():
         package="flight_control",
         executable="flight_control_node",
         parameters=[
-            {"mission_bt_file"         : "./src/my_robot/behaviour_trees/takeoff_up_picture_down_land.xml"},
+            {"mission_bt_file"         : "./src/my_robot/behaviour_trees/flight.xml"},
             {"navigation_bt_file"      : "./src/navigation_lite/behavior_trees/navigate.xml"},            
             {"minimum_battery_voltage" : 13.6}
         ]
